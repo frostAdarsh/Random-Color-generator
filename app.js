f412e39be5cdad0e305dae6e907fce87394eb7bd
frostAdarsh <adarshmani017@gmail.com>
2024-04-20 17:23:22 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const getColor =()=>{
+    const randomNumber = Math.floor(Math.random()*16777215);
+    const randomColor = '#'+randomNumber.toString(16);
+    // console.log(randomNumber,randomColor)
+    document.body.style.backgroundColor = randomColor;
+    document.getElementById('color_code').innerText = randomColor;
+
+    navigator.clipboard.writeText(randomColor)
+}
+document.getElementById('btn').addEventListener(
+    'click',
+    getColor
+)
+
+getColor();
